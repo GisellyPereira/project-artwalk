d73264d1566927ee449de67ca7ea20625606a300
giselly <gisellyavp@gmail.com>
2025-09-10 11:57:00 -0300

    :construction: feat: construindo o header

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../css/style.css";
 
-
 import { logoArtwalk } from "./imgs";
 
 function renderImageHeader() {
-    const imgLogo = document.querySelector(".logo-artwalk-header");
-    imgLogo.src = logoArtwalk;
+  const imgLogos = document.querySelectorAll(".logo-artwalk-header");
+  
+  imgLogos.forEach(img => {
+    img.src = logoArtwalk;
+  });
 }
 
 renderImageHeader();
-
-
